f3a58bd97db9e0162278e6e814364c994736e10e
aryadimade <madearyadi@gmail.com>
2019-07-26 17:12:49 +0700

    add disabled room button when there is already 2 players in a particular room

--- app.js
+++ app.js
@@ -11,15 +11,19 @@ app.get('/', (req, res, next) => {
 io.on('connection', function(socket){
   console.log('a user connected');
   
-  socket.on('join-room-1', function(){
+  socket.on('join-room-1', function(cb){
     if (!io.nsps['/'].adapter.rooms['room-1']) {
       socket.join('room-1');
+
+      // console.log('dari server',io.nsps['/'].adapter);
       console.log('masuk room 1');
+      cb(true)
     } else if (io.nsps['/'].adapter.rooms['room-1']['length'] < 2) {
       socket.join('room-1');
       console.log('masuk room 1');
+      cb(true)
     } else {
-      console.log('udah penuh bang');
+      cb(false)
     }
   })
 
@@ -27,8 +31,19 @@ io.on('connection', function(socket){
     let roomsKeys = Object.keys(io.nsps['/'].adapter.rooms);
     let roomName = roomsKeys[roomsKeys.length-1];
     let playerAmount = io.nsps['/'].adapter.rooms[roomName]['length'];
-    io.to(roomName).emit('checkPlayer', playerAmount);
+    io.emit('checkPlayer', playerAmount);
   });
+
+  socket.on('checkBeforeEnter', function(value){
+    if (io.nsps['/'].adapter.rooms[value] == undefined) {
+      // console.log(`${value} ADUH undefineed`);
+      io.emit(`checkBeforeEnter-${value}`, 0);
+    } else {
+      // console.log(`${value} dia ENGGA undefineed`);
+      let playerAmount = io.nsps['/'].adapter.rooms[value]['length'];
+      io.emit(`checkBeforeEnter-${value}`, playerAmount);
+    }
+  })
 
   socket.on('increment', function(value){
     let roomsKeys = Object.keys(io.nsps['/'].adapter.rooms);
@@ -36,23 +51,28 @@ io.on('connection', function(socket){
     io.to(roomName).emit('increment', value);
   })
 
-  socket.on('join-room-2', function(){
+  socket.on('join-room-2', function(cb){
     if (!io.nsps['/'].adapter.rooms['room-2']) {
       socket.join('room-2');
       console.log('masuk room 2');
+      cb(true)
     } else if (io.nsps['/'].adapter.rooms['room-2']['length'] < 3) {
       socket.join('room-2');
+      cb(true)
       console.log('masuk room 2');
+    } else {
+      cb(false)
     }
+  })
+  
+  socket.on('setCounter', function() {
+    io.emit('setCounter');
   })
 
   socket.on('disconnect', function() {
     console.log('a user disconnected')
   })
 
-  socket.on('setCounter', function(value) {
-    io.emit('setCounter');
-  })
 });
 
 http.listen(port, function(){
