5a06a44516f29259cebfaf0bb5e436700e44c13d
aryadimade <madearyadi@gmail.com>
2019-07-26 13:40:06 +0700

    make socket on increment more dynamic

--- app.js
+++ app.js
@@ -21,15 +21,14 @@ io.on('connection', function(socket){
       console.log('masuk room 1');
     } else {
       console.log('udah penuh bang');
-      
     }
-    // socket.join('room-1');
-    // console.log(io.nsps['/'].adapter.rooms['room-1']['length']);
     
   })
 
-  socket.on('increment-1', function(){
-    io.to('room-1').emit('increment');
+  socket.on('increment', function(value){
+    let roomsKeys = Object.keys(io.nsps['/'].adapter.rooms);
+    let roomName = roomsKeys[roomsKeys.length-1];
+    io.to(roomName).emit('increment', value);
   })
 
   socket.on('join-room-2', function(){
@@ -41,12 +40,6 @@ io.on('connection', function(socket){
       socket.join('room-2');
       console.log('masuk room 2');
     }
-    // socket.join('room-2');
-    // console.log(io.nsps['/'].adapter.rooms['room-2']['length']);
-  })
-
-  socket.on('increment-2', function(){
-    io.to('room-2').emit('increment');
   })
 
   socket.on('disconnect', function() {
