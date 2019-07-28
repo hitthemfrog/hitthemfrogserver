f49230cdcb947931d9a0c233d129e26736100350
aryadimade <madearyadi@gmail.com>
2019-07-28 17:57:56 +0700

    add delete player in a room when the player disconnected

--- app.js
+++ app.js
@@ -12,15 +12,25 @@ const setPlayerScore = require('./listener/setplayerscore')
 
 
 const appRoom = {}
-  
+const activePlayer = {}
+
 
 io.on('connection', function (socket) {
   console.log('a user connected');
   const nsp = io.of('/')
   const socketRooms = nsp.adapter.rooms
 
-  socket.on('joinRoom', joinRoom({ io, socket, socketRooms, appRoom }))
-  socket.on('setPlayerScore', setPlayerScore({ io, appRoom}))
+  socket.on('joinRoom', joinRoom({
+    activePlayer,
+    io,
+    socket,
+    socketRooms,
+    appRoom
+  }))
+  socket.on('setPlayerScore', setPlayerScore({
+    io,
+    appRoom
+  }))
   socket.on('checkRoom', () => {
     console.log('emitted')
     let roomKeys = Object.keys(appRoom)
@@ -29,6 +39,30 @@ io.on('connection', function (socket) {
   })
 
   socket.on('disconnect', function () {
+    if (activePlayer[socket.id]) {
+    //   console.log('dari socket disconnect active player', activePlayer[socket.id]);
+    //   console.log('dari socket disconnect with room', activePlayer[socket.id]['roomName']);
+      let roomName = activePlayer[socket.id].roomName;
+      let playerName = activePlayer[socket.id].playerName;
+      console.log('INI NAMA PEMAIN', playerName);
+      
+      if (appRoom[roomName].players.length == 1) {
+        delete appRoom[roomName];
+        let roomKeys = Object.keys(appRoom)
+        let rooms = roomKeys.map(e => appRoom[e])
+        io.emit('listRoom', rooms)
+      } 
+      else if (appRoom[roomName].players.length == 2) {
+        let playerIndex = appRoom[roomName].players.findIndex(e => e.name === playerName);
+        appRoom[roomName].players.splice(playerIndex, 1);
+        console.log('check room kehapus belom,', appRoom[roomName].players);
+        let roomKeys = Object.keys(appRoom)
+        let rooms = roomKeys.map(e => appRoom[e])
+        io.emit('listRoom', rooms)
+      }
+    }
+
+    
     console.log('a user disconnected')
   })
 
--- listener/joinroom.js
+++ listener/joinroom.js
@@ -1,28 +1,58 @@
 // template listener
 // export sebuah function
 // kemudian return sebuah function yang menerima param data & callback
-const { isPlayerInRoom } = require('../helpers')
-const { createRooms, createPlayer } = require('../types')
+const {
+  isPlayerInRoom
+} = require('../helpers')
+const {
+  createRooms,
+  createPlayer
+} = require('../types')
 
 module.exports = ({
-  io, socket, socketRooms, appRoom
+  activePlayer,
+  io,
+  socket,
+  socketRooms,
+  appRoom
 }) => {
-  
-  return (data, cb) => {
-    let { roomName, playerName } = data
 
-    console.log({ roomName, playerName, cb })
+  return (data, cb) => {
+    let {
+      roomName,
+      playerName
+    } = data
+
+    // console.log({ roomName, playerName, cb })
     let selectedRoom = socketRooms[roomName]
     if (!selectedRoom) {
       socket.join(roomName)
       appRoom[roomName] = createRooms(roomName, playerName)
+      activePlayer[socket.id] = {
+        playerName,
+        roomName
+      }
+
+      console.log('di dalam join room, player name', activePlayer[socket.id].playerName);
+      // console.log('ini activePlayer'. activePlayer);
       cb(true)
     } else if (selectedRoom && selectedRoom.length < 2 && !isPlayerInRoom(appRoom[roomName], playerName)) {
+      socket.join(roomName)
       appRoom[roomName].players.push(createPlayer(playerName))
+      activePlayer[socket.id] = {
+        playerName,
+        roomName
+      }
+      // console.log('ini activePlayer');
+
+      console.log('di dalam join room, player name', activePlayer[socket.id].playerName);
       cb(true)
     } else {
       cb(false)
     }
+
+    // console.log('ini activePlayer', activePlayer);
+
     let roomKeys = Object.keys(appRoom)
     let rooms = roomKeys.map(e => appRoom[e])
     io.emit('listRoom', rooms)
