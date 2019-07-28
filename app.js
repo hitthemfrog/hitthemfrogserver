904b95fc75251de92dbe479335689629f41bd692
sukmaranggapradeta <adit.netral@gmail.com>
2019-07-28 14:47:28 +0700

    multer upload image

--- app.js
+++ app.js
@@ -1,20 +1,59 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const cors = require('cors')
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
-
-const players = {
-  'abc': {
-
+//buat upload image
+const Multer = require('multer');
+const multer = Multer({
+    storage: Multer.MemoryStorage,
+    limits: {
+        fileSize: 10 * 1024 * 1024, // Maximum file size is 10MB
+    },
+});
+const multerConf = {
+  storage: Multer.diskStorage({
+  destination: function (req, file, next) {
+    next(null, './public/user/picture')
+  },
+  filename: function (req, file, next) {
+      console.log(req)
+      let { socketId, roomName } = req.body
+      const ext = file.mimetype.split('/')[1]
+      next(null, `${socketId}-${roomName}.${ext}`);
   }
+}),
+fileFilter: function(req, file, next){
+    if (!file){
+        next();
+    }
+    const image = file.mimetype.startsWith('image/');
+    if (image){
+        next(null, true)
+    }
+    else{
+        next({message:"File type not supported"}, false)
+    }
 }
-
+}
 const avail_room = []
+
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
 
 app.get('/', (req, res, next) => {
   res.status(200).json('HELLO HIT THEM FROG');
 })
+
+app.post("/uploadimage", Multer(multerConf).single('image'), (req, res, next) => {
+  // console.log(req.body, " ini req.body")
+
+  console.log(req.file, " ini req.file")
+});
+
+
 
 io.on('connection', function(socket){
   console.log('a user connected');
@@ -25,6 +64,22 @@ io.on('connection', function(socket){
   //   // io.emit('checkRooms', avail_room)
   // })
 
+  socket.on('getPlayersInROom', function(value) {
+    console.log('getPlayersInROom ke trigger')
+    console.log('value' , value)
+    let playerInRoom = avail_room.filter(el => (el.name === value.roomname ))
+    // avail_room.forEach(el => {
+    //   console.log(el.name, "----------")
+    // })
+    console.log(playerInRoom, " player")
+    io.emit('getPlayersInROom', playerInRoom)
+  })
+
+  socket.on('listRooms', function() {
+    console.log('listRooms ke trigger')
+    io.emit('listRooms', avail_room)
+  })
+
   socket.on('createRooms', function(data) {
     console.log('createRooms ke trigger')
     // let roomsObject = io.sockets.adapter.rooms
@@ -34,6 +89,7 @@ io.on('connection', function(socket){
     console.log(data)
     avail_room.push({
       name: data.roomName,
+      status: 'waiting',
       players: [
         {
           playerName: data.player,
