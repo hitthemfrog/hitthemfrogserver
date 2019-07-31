397dfcaa05381f8816c8be7bc8674ddc19426372
aryadimade <madearyadi@gmail.com>
2019-07-31 11:59:42 +0700

    fix wrong room name in the test file

--- test/app.test.js
+++ test/app.test.js
@@ -437,7 +437,7 @@ describe('Socket test suit', function () {
     return new Promise((resolve, reject) => {
       socket1.on('listRoom', (rooms) => {
         try {
-          expect(rooms[0].name).toEqual('room8')
+          expect(rooms[0].name).toEqual('room1')
         } catch (err) {
           reject(err)
         }
@@ -445,11 +445,11 @@ describe('Socket test suit', function () {
       })
 
       socket1.emit(`joinRoom`, {
-        roomName: 'room8',
+        roomName: 'room1',
         playerName: 'playerName'
       }, (value) => {
         expect(value).toEqual(true);
-        socket1.emit('leaveRoom', 'room8')
+        socket1.emit('leaveRoom', 'room1')
       })
 
     })
