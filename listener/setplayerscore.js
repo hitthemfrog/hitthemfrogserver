5d159af5217b780a95aaf56ce7f13c7fbfec9d1f
Kharis Khasburrahman <khasburrahman@yahoo.com>
2019-07-31 17:52:08 +0700

    ganti cara game selesai

--- listener/setplayerscore.js
+++ listener/setplayerscore.js
@@ -20,25 +20,39 @@ module.exports  = ({
     let index = room.players.findIndex(p => p.name === playerDataObj.player)
     let player1 = room.players[0]
     let player2 = room.players[1]
-    let allPlayerActive = player1 && player2 
+    
+    let allPlayerActive = player1 && player2
     let activePlayer = room.players[index];
+    let activePlayerTotalEmit = activePlayer && activePlayer.hit + activePlayer.miss;
 
-    if (activePlayer) {
+    if (activePlayer && activePlayerTotalEmit <= 5) {
       activePlayer.hit = playerDataObj.hit
       activePlayer.miss = playerDataObj.miss
     }
+
+    let player1score  
+    let player1totalEmit
+    if (player1) {
+      player1totalEmit = player1.hit + player1.miss
+      player1score = player1.hit - player1.miss
+      
+    }
+    let player2score
+    let player2totalEmit
+    if (player2) {
+      player2totalEmit = player2.hit + player2.miss
+      player2score = player2.hit - player2.miss
+    }
     
-    if (allPlayerActive && player1.miss == '5') {
-      winner = player2.name
-      emitIsGameFinished(io, playerDataObj.room, winner, room.players)
-    } else if (allPlayerActive && player2.miss == '5') {
-      winner = player1.name
-      emitIsGameFinished(io, playerDataObj.room, winner, room.players)
-    } else if (allPlayerActive && player1.hit == '10') {
-      winner = player1.name
-      emitIsGameFinished(io, playerDataObj.room, winner, room.players)
-    } else if (allPlayerActive && player2.hit == '10') {
-      winner = player2.name
+    
+    // console.log({player1totalEmit, player2totalEmit, player1score, player2score})
+    
+    if (allPlayerActive && player1totalEmit >= 5 && player2totalEmit >= 5) {
+      if (player1score > player2score) {
+        winner = player1.name
+      } else {
+        winner = player2.name
+      }
       emitIsGameFinished(io, playerDataObj.room, winner, room.players)
     }
 
